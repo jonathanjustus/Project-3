0e2fa991c29ca09cf36abcc7353c197d8d9f7d55
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-03-30 17:03:39 -0500

    MesoAsciiAbstract Class Javadoc

--- MesoAsciiAbstract.java
+++ MesoAsciiAbstract.java
@@ -1,4 +1,15 @@
+/**
+ * MesoAsciiAbstract Class
+ * 
+ * @author johny
+ * @version 3-30-19
+ *
+ */
 public abstract class MesoAsciiAbstract
 {
+	/**
+	 * Abstract calAverage Method
+	 * @return int
+	 */
 	abstract int calAverage();
 }
