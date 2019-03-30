b0eaedf81d1b3f840ac031b3042b1aa20f888a92
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-03-30 17:48:06 -0500

    Main Class

--- Main.java
+++ Main.java
@@ -1,4 +1,39 @@
+/**
+ * Main Class
+ * 
+ * @author johny
+ * @version 3-30-19
+ */
+import java.io.IOException;
+import java.util.HashMap;
 
-public class Main {
-
+public class Main 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		String StID = "FAIR";
+		MesoAscii AsciiAverage = new MesoAscii(new MesoStation(StID));
+		int AsciiAvg = AsciiAverage.calAverage();
+		
+		HashMap<String, Integer> AsciiValues = new HashMap<String, Integer>();
+		AsciiValues.put(StID, AsciiAvg);
+		System.out.print("ASCII average: ");
+		System.out.println(AsciiValues.get(StID));
+		
+		MesoEqual mesoequal = new MesoEqual(StID);
+		System.out.println("Stations are: "+ mesoequal.calAsciiEqual());
+		
+		System.out.print("##");
+		System.out.println("Unsroted:");
+		AsciiValues = mesoequal.calAsciiEqual();
+		
+		for (String stid : AsciiValues.keySet()) 
+		{
+		    System.out.println(stid + " " + AsciiValues.get(stid));		    
+		}
+		
+		System.out.print("##");
+		System.out.println("Sorted:");
+		new StationLexicographical(AsciiValues);	  
+	}
 }
