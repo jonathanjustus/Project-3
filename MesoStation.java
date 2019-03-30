18537f0f3486ee95c811fda27b86c1b549bf4ed9
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-03-30 16:58:56 -0500

    MesoStation Final

--- MesoStation.java
+++ MesoStation.java
@@ -1,17 +1,31 @@
-
+/**
+ * MesoStation Class
+ * 
+ * @author johny
+ * @version 3-30-19
+ */
 public class MesoStation 
 {
+	/**
+	 * Instance Variable
+	 */
 	private String StID;
 	
+	/**
+	 * Constructor
+	 * @param String StID
+	 */
 	public MesoStation(String StID)
 	{
 		this.StID = StID;
 	}
-
+	
+	/**
+	 * GetStID Method
+	 * @return StID
+	 */
 	public String getStID()
 	{
 		return StID;
 	}
-	
-	
 }
