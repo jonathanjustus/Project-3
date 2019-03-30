bbe04ed53bb7b97978baa5529454cca793d0ccbd
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-03-30 16:58:08 -0500

    Javadoc for MesoStation

--- MesoStation.java
+++ MesoStation.java
@@ -1,4 +1,17 @@
 
-public class MesoStation {
+public class MesoStation 
+{
+	private String StID;
+	
+	public MesoStation(String StID)
+	{
+		this.StID = StID;
+	}
 
+	public String getStID()
+	{
+		return StID;
+	}
+	
+	
 }
