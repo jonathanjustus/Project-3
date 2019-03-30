4d49177290838a0883ce5ebc908e0027c4635a3c
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-03-30 17:42:30 -0500

    StationLexicographical with Javadoc

--- StationLexicographical.java
+++ StationLexicographical.java
@@ -1,3 +1,13 @@
+/**
+ * StationLexicographical Class (extends MesoSortedAbstract)
+ * 
+ * @author johny
+ * @version 3-30-19
+ */
+
+/**
+ * Import Statements
+ */
 import java.util.HashMap;
 import java.util.Map;
 import java.util.SortedMap;
@@ -5,14 +15,26 @@ import java.util.TreeMap;
 
 public class StationLexicographical extends MesoSortedAbstract
 {
+	/**
+	 * Instance Variable
+	 */
 	private HashMap<String, Integer> asciiValues;
 	
+	/**
+	 * Constructor
+	 * @param asciiValues
+	 */
 	public StationLexicographical(HashMap<String, Integer> asciiValues)
 	{
 		this.asciiValues = asciiValues;
 		sortedMap(asciiValues);
 	}
 	
+	/**
+	 * sortedMap Method
+	 * @param unsortedHashMap
+	 * @return sortedMap
+	 */
 	public Map<String, Integer> sortedMap(HashMap<String, Integer> unsortedHashMap)
 	{
 		SortedMap<String, Integer> sortedMap = new TreeMap<String, Integer>();
