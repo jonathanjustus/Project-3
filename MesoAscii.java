e3e57de05e7e301809db57e34d7f718f4b9283a0
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-03-30 17:48:31 -0500

    Project 3 Final Version

--- MesoAscii.java
+++ MesoAscii.java
@@ -1,4 +1,38 @@
 
-public class MesoAscii {
-
+public class MesoAscii extends MesoAsciiAbstract
+{
+	private MesoStation mesoStation;
+	
+	public MesoAscii(MesoStation mesoStation)
+	{	
+		this.mesoStation = mesoStation;
+	}
+	
+	public int calAverage()
+	{	
+		int intAvg = mesoStation.getStID().length();
+		double asciiAdded = 0;
+		String mesoID = mesoStation.getStID();
+		
+		for (int i = 0; i < mesoID.length(); ++i)
+		{
+			asciiAdded += mesoID.charAt(i);
+		}
+		
+		double asciiAvg = asciiAdded / mesoID.length();
+		
+		int asciiCeiling = (int) Math.ceil(asciiAvg);
+		int asciiFloor = (int) Math.floor(asciiAvg);
+		
+		if ((Math.abs(asciiAvg - asciiCeiling)) <= (Math.abs(asciiFloor - asciiAvg)))
+		{
+			intAvg = asciiCeiling;
+		}
+		else
+		{
+			intAvg = asciiFloor;
+		}
+		
+		return intAvg;
+	}
 }
