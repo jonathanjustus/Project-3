4525f563fe6bd1a3e6a1b8073fec3703e8462d50
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-03-30 17:31:03 -0500

    MesoSortedAbstract Javadoc

--- MesoSortedAbstract.java
+++ MesoSortedAbstract.java
@@ -1,7 +1,19 @@
+/**
+ * MesoSortedAbstract Class
+ * 
+ * @author johny
+ * @version 3-30-19
+ */
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class MesoSortedAbstract
 {
+	/**
+	 * Abstract Method sortedMap
+	 * 
+	 * @param unsorted
+	 * @return Map<String, Integer>
+	 */
 	abstract Map<String, Integer> sortedMap(HashMap<String, Integer> unsorted);
 }
