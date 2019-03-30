b74c5fa548f960681015aa2abcdf28c9be66e233
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-03-30 17:38:32 -0500

    Create sortedMap method

--- StationLexicographical.java
+++ StationLexicographical.java
@@ -13,6 +13,17 @@ public class StationLexicographical extends MesoSortedAbstract
 	
 	public Map<String, Integer> sortedMap(HashMap<String, Integer> unsortedHashMap)
 	{
+		SortedMap<String, Integer> sortedMap = new TreeMap<String, Integer>();
+		
+		for (String stationID : asciiValues.keySet())
+		{
+			sortedMap.put(stationID, asciiValues.get(stationID));
+		}
+		
+		for (String stationID : sortedMap.keySet())
+		{
+			System.out.println(stationID + " " + sortedMap.get(stationID));
+		}
 		
 		return sortedMap;
 	}
