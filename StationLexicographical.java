69c15302e26e3c1be523fa92634628e053100dea
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-03-30 17:36:17 -0500

    Constructor

--- StationLexicographical.java
+++ StationLexicographical.java
@@ -4,5 +4,10 @@ public class StationLexicographical extends MesoSortedAbstract
 {
 	private HashMap<String, Integer> asciiValues;
 	
+	public StationLexicographical(HashMap<String, Integer> asciiValues)
+	{
+		this.asciiValues = asciiValues;
+		sortedMap(asciiValues);
+	}
 	
 }
